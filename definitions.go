afbe6d00bad7010504d73f32ec55d544d4d3b1eb
Lorenceau Pablo <pablo.lorenceau@elca.ch>
2020-12-04 10:48:29 +0100

    [SCPR-32] Add SCPR email representation

--- definitions.go
+++ definitions.go
@@ -757,3 +757,32 @@ type RecoveryCodeRepresentation struct {
 type ActivationCodeRepresentation struct {
 	Code *string `json:"code,omitempty"`
 }
+
+// CPREmailRepresentation struct
+type CPREmailRepresentation struct {
+	Recipient   *string                         `json:"recipient,omitempty"`
+	Theming     *CPREmailThemingRepresentation  `json:"theming,omitempty"`
+	Attachments *[]CPRAttachementRepresentation `json:"attachments,omitempty"`
+}
+
+// CPREmailThemingRepresentation struct
+type CPREmailThemingRepresentation struct {
+	SubjectKey         *string                              `json:"subjectKey,omitempty"`
+	SubjectParameters  *[]string                            `json:"subjectParameters,omitempty"`
+	Template           *string                              `json:"template,omitempty"`
+	TemplateParameters *CPRTemplateParametersRepresentation `json:"templateParameters,omitempty"`
+	Locale             *string                              `json:"locale,omitempty"`
+}
+
+// CPRTemplateParametersRepresentation struct
+type CPRTemplateParametersRepresentation struct {
+	Name   *string `json:"name,omitepmty"`
+	Result *string `json:"result,omitepmty"`
+}
+
+// CPRAttachementRepresentation struct
+type CPRAttachementRepresentation struct {
+	Filename    *string `json:"filename,omitempty"`
+	ContentType *string `json:"contentType,omitempty"`
+	Content     *string `json:"content,omitempty"`
+}
